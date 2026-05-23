edb5c38ce954bd4da82617652ab2dbfb7d15413b
Bhaskar-06 <abibhaskar1009@gmail.com>
2026-05-23 12:50:14 +0530

    Add vercel config and env for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,12 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://my-map-i29y.onrender.com',
         changeOrigin: true,
       }
     }
+  },
+  build: {
+    outDir: 'dist',
   }
 })
